b87379404a8be9f399a1f7f9a09e5ccc9086a0a5
ARYAN CHOUDHARY <f20230918@goa.bits-pilani.ac.in>
2023-10-22 04:26:08 +0530

    changes added

---
+++ ass1b.py
@@ -0,0 +1,6 @@
+import math
+n=input("enter a number: ")
+f=1
+for i in range(1,int(n)+1):
+    f=f*i
+print("factorial value of the number is: "+ str(f))
---
+++ ass1c.py
@@ -0,0 +1,5 @@
+yr=input("enter the year: ")
+if int(yr)%4==0:
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
